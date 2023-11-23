d037aae275166576561eacf0ccee497cd1d414d4
yhzhu <1107728123@qq.com>
2023-11-23 18:48:27 +0800

    can get ntp

--- src/main.c
+++ src/main.c
@@ -1,175 +1,62 @@
-/* Networking DHCPv4 client */
-
 /*
- * Copyright (c) 2017 ARM Ltd.
- * Copyright (c) 2016 Intel Corporation.
- *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(net_dhcpv4_client_sample, LOG_LEVEL_DBG);
-
 #include <zephyr/kernel.h>
-#include <zephyr/linker/sections.h>
-#include <errno.h>
-#include <stdio.h>
+#include <zephyr/posix/time.h>
+#include <zephyr/net/sntp.h>
 
-#include <zephyr/net/net_if.h>
-#include <zephyr/net/net_core.h>
-#include <zephyr/net/net_context.h>
-#include <zephyr/net/net_mgmt.h>
-#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(wifi_net, LOG_LEVEL_DBG);
 
-#define DHCP_OPTION_NTP (42)
+#include "wifi_mgr.h"
 
-static uint8_t ntp_server[4];
-static char buf[NET_IPV4_ADDR_LEN];
+#define SNTP_SERVER             "0.pool.ntp.org"
+#define SNTP_RETRY_TIME         (5)
 
-K_SEM_DEFINE(wifi_conn_sem, 0, 1);
-K_SEM_DEFINE(ipv4_sem, 0, 1);
-
-static struct net_mgmt_event_callback wifi_cb;
-static struct net_mgmt_event_callback mgmt_cb;
-
-static struct net_dhcpv4_option_callback dhcp_cb;
-
-static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
+static uint64_t do_sntp(void)
 {
-    const struct wifi_status *status = (const struct wifi_status *)cb->info;
+	int rc;
+	struct sntp_time sntp_time;
+	char time_str[sizeof("1970-01-01T00:00:00")];
 
-    if (status->status)
-    {
-        LOG_ERR("Connection request failed (%d)", status->status);
-    }
-    else
-    {
-        LOG_INF("Connected");
-        k_sem_give(&wifi_conn_sem);
-    }
-}
+    int cnt = 0;
+    do {
+        LOG_INF("Sending NTP request for current time:");
+        rc = sntp_simple(SNTP_SERVER, SYS_FOREVER_MS, &sntp_time);
+        if (rc == 0) {
+            time_t now = sntp_time.seconds;
+            struct tm now_tm;
 
-static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
-{
-    switch (mgmt_event)
-    {
-
-        case NET_EVENT_WIFI_CONNECT_RESULT:
-			handle_wifi_connect_result(cb);
+            gmtime_r(&now, &now_tm);
+            strftime(time_str, sizeof(time_str), "%FT%T", &now_tm);
+            LOG_INF("  Acquired time: %s", time_str);
+            LOG_INF("  sec: %lld", sntp_time.seconds);
             break;
+        } else {
+            LOG_ERR("  Failed to acquire SNTP, code %d,  cnt:%d\n", rc, cnt);
+        }
+        cnt++;
+    } while (cnt < SNTP_RETRY_TIME);
 
-        case NET_EVENT_WIFI_DISCONNECT_RESULT:
-            handle_wifi_connect_result(cb);
-            break;
-
-        default:
-            break;
+    if (rc == 0) {
+        return sntp_time.seconds;
+    } else {
+        return -1;
     }
 }
-
-static void start_dhcpv4_client(struct net_if *iface, void *user_data)
-{
-	ARG_UNUSED(user_data);
-
-	LOG_INF("Start on %s: index=%d", net_if_get_device(iface)->name,
-		net_if_get_by_iface(iface));
-	net_dhcpv4_start(iface);
-}
-
-static void handler(struct net_mgmt_event_callback *cb,
-		    uint32_t mgmt_event,
-		    struct net_if *iface)
-{
-	int i = 0;
-
-	if (mgmt_event != NET_EVENT_IPV4_ADDR_ADD) {
-		return;
-	}
-
-	for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
-
-		if (iface->config.ip.ipv4->unicast[i].addr_type !=
-							NET_ADDR_DHCP) {
-			LOG_WRN("%s %d, addr_type: %d", __FUNCTION__, __LINE__, iface->config.ip.ipv4->unicast[i].addr_type);
-			continue;
-		}
-
-		LOG_INF("   Address[%d]: %s", net_if_get_by_iface(iface),
-			net_addr_ntop(AF_INET, &iface->config.ip.ipv4->unicast[i].address.in_addr, buf, sizeof(buf)));
-		LOG_INF("    Subnet[%d]: %s", net_if_get_by_iface(iface),
-			net_addr_ntop(AF_INET, &iface->config.ip.ipv4->netmask, buf, sizeof(buf)));
-		LOG_INF("    Router[%d]: %s", net_if_get_by_iface(iface),
-			net_addr_ntop(AF_INET, &iface->config.ip.ipv4->gw, buf, sizeof(buf)));
-		LOG_INF("Lease time[%d]: %u seconds", net_if_get_by_iface(iface),iface->config.dhcpv4.lease_time);
-
-		k_sem_give(&ipv4_sem);
-	}
-}
-
-static void option_handler(struct net_dhcpv4_option_callback *cb,
-			   size_t length,
-			   enum net_dhcpv4_msg_type msg_type,
-			   struct net_if *iface)
-{
-	char buf[NET_IPV4_ADDR_LEN];
-
-	LOG_INF("DHCP Option %d: %s", cb->option,
-		net_addr_ntop(AF_INET, cb->data, buf, sizeof(buf)));
-}
-
-static void wifi_connect(const char *ssid, const char *passwd)
-{
-    struct net_if *iface = net_if_get_default();
-
-    struct wifi_connect_req_params wifi_params = {0};
-
-    wifi_params.ssid = ssid;
-    wifi_params.psk = passwd;
-    wifi_params.ssid_length = strlen(ssid);
-    wifi_params.psk_length = strlen(passwd);
-    wifi_params.channel = WIFI_CHANNEL_ANY;
-    wifi_params.security = WIFI_SECURITY_TYPE_PSK;
-    wifi_params.band = WIFI_FREQ_BAND_2_4_GHZ; 
-    wifi_params.mfp = WIFI_MFP_OPTIONAL;
-
-    LOG_INF("Connecting to SSID: %s", wifi_params.ssid);
-
-    if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &wifi_params, sizeof(struct wifi_connect_req_params)))
-    {
-        LOG_INF("WiFi Connection Request Failed");
-    }
-}
-
 
 int main(void)
 {
-	LOG_INF("Run dhcpv4 client");
+	LOG_INF("hello world, %s", CONFIG_BOARD);
+	// wifi_init("TreeNewbear", "csk_wifi_connect");
+	wifi_init("yhzhu", "12345678");
 
-	net_mgmt_init_event_callback(&wifi_cb, wifi_mgmt_event_handler,
-								NET_EVENT_WIFI_CONNECT_RESULT | NET_EVENT_WIFI_DISCONNECT_RESULT);
-	net_mgmt_add_event_callback(&wifi_cb);
-
-	net_mgmt_init_event_callback(&mgmt_cb, handler,
-				     NET_EVENT_IPV4_ADDR_ADD);
-	net_mgmt_add_event_callback(&mgmt_cb);
-
-	k_msleep(5000);		// esp32 init take too long time, must started before connect
-
-	wifi_connect("TreeNewbear", "csk_wifi_connect");
-	k_sem_take(&wifi_conn_sem, K_FOREVER);
-
-	net_dhcpv4_init_option_callback(&dhcp_cb, option_handler,
-					DHCP_OPTION_NTP, ntp_server,
-					sizeof(ntp_server));
-	net_dhcpv4_add_option_callback(&dhcp_cb);
-
-	net_if_foreach(start_dhcpv4_client, NULL);
-
-	k_sem_take(&ipv4_sem, K_FOREVER);
+	do_sntp();
 
 	while(1){
-		k_msleep(1000);
-		LOG_INF("hello world");
+		k_msleep(10000);
+		LOG_INF("Hello world");
 	}
 
 	return 0;
